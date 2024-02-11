9d46311d59ab417966bf7be54d1641b9359616ca
Arkhipov Alex <59509425+arhipco@users.noreply.github.com>
2024-02-11 21:33:05 +0100

    added music on background

--- Numbers/script.js
+++ Numbers/script.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
- 
-const gradient = ctx.createLinearGradient(0,0,canvas.width, canvas.height);
+
+const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
 gradient.addColorStop(0, 'white');
 gradient.addColorStop(0.5, 'gold');
 gradient.addColorStop(1, 'orangered');
@@ -13,10 +13,10 @@ ctx.font = "bold 20px Courier";
 let currentNumber = 1;
 let resetGame = false;
 
-let printTXT = "Order numbers";
+let printTXT = "Press 'm' to play/pause music. Looking number:";
 
 class Particle {
-    constructor(player, number){
+    constructor(player, number) {
         this.player = player;
         this.number = number;
         this.ratioIndex = 50;
@@ -30,54 +30,50 @@ class Particle {
         if (this.vx == 0) this.vx = 1;
         if (this.vy == 0) this.vy = 1;
     }
-    draw(context){
-        if(this.isSelected){
+    draw(context) {
+        if (this.isSelected) {
             context.fillStyle = "green";
-        }else{
+        } else {
             context.fillStyle = gradient;
-        } 
+        }
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         context.fill();
         context.stroke();
         context.fillStyle = 'black'; // print Number
-        context.fillText(this.number, this.x - (5 * this.number.toString().length), this.y + 5); 
-        //context.stroke();
+        context.fillText(this.number, this.x - (5 * this.number.toString().length), this.y + 5);
     }
-    update(){
-        if(player.mouse.pressed){ 
-            const distance = Math.hypot((player.mouse.x - this.x),(player.mouse.y - this.y));
+    update() {
+        if (player.mouse.pressed) {
+            const distance = Math.hypot((player.mouse.x - this.x), (player.mouse.y - this.y));
 
-            if (distance <= this.radius){
-                if(this.number == currentNumber + 1) {
+            if (distance <= this.radius) {
+                if (this.number == currentNumber + 1) {
                     currentNumber++;
                     this.isSelected = true;
-                }else if(!resetGame && !this.isSelected){ 
-                    resetGame = true; 
-                } 
+                } else if (!resetGame && !this.isSelected) {
+                    resetGame = true;
+                }
             }
         }
-        if(this.isSelected){
+        if (this.isSelected) {
             this.radius = this.player.width / this.ratioIndex + 20;
         }
         this.x += this.vx;
         this.y += this.vy;
 
         if (this.x < this.radius) this.vx = Math.random();
-            
-        if ( this.x > this.player.width - this.radius) this.vx = -Math.random(); 
-        
+        if (this.x > this.player.width - this.radius) this.vx = -Math.random();
         if (this.y < this.radius) this.vy = Math.random();
-            
-        if ( this.y > this.player.height - this.radius) this.vy = -Math.random();
+        if (this.y > this.player.height - this.radius) this.vy = -Math.random();
     }
-    reset(){
-        if(this.number == currentNumber){
+    reset() {
+        if (this.number == currentNumber) {
             this.isSelected = true;
-        }else{
+        } else {
             this.isSelected = false;
         }
-        
+
         this.radius = Math.floor(Math.random() * 10 + this.player.width / this.ratioIndex);
         this.x = this.radius + Math.random() * (this.player.width - this.radius * 3);
         this.y = this.radius + Math.random() * (this.player.height - this.radius * 3); // baseRadius?
@@ -85,7 +81,7 @@ class Particle {
 }
 
 class Player {
-    constructor(canvas, context){
+    constructor(canvas, context) {
         this.canvas = canvas;
         this.context = context;
         this.width = this.canvas.width;
@@ -100,11 +96,11 @@ class Player {
             y: 0,
             pressed: false
         }
-
+        
         window.addEventListener('resize', e => {
             this.resize(e.target.window.innerWidth, e.target.window.innerHeight);
         });
-        window.addEventListener('touchstart', e => { 
+        window.addEventListener('touchstart', e => {
             this.mouse.pressed = true;
             this.mouse.x = e.changedTouches[0].pageX;
             this.mouse.y = e.changedTouches[0].pageY;
@@ -117,30 +113,43 @@ class Player {
         window.addEventListener('mouseup', e => {
             this.mouse.pressed = false;
         });
+        window.addEventListener('keydown', e => {
+            if(e.key == 'm') this.toggleMusic();
+        });
     }
-    createParticles(){
-        for (let i = 0; i < this.numberOfParticles; i++){
-            this.particles.push(new Particle(this, i+1));
+    toggleMusic() {
+        this.bgMusic = document.getElementById('bg-music'); 
+        if(this.bgMusic.paused) {
+          this.bgMusic.play();  
+        } else {
+          this.bgMusic.pause();
+        }
+      }
+    createParticles() {
+        for (let i = 0; i < this.numberOfParticles; i++) {
+            this.particles.push(new Particle(this, i + 1));
         }
     }
-    updateParticles(context){
+    updateParticles(context) {
         context.fillStyle = 'yellow';
-        context.fillText((printTXT), 30, this.canvas.height - 20);
-        if(resetGame){
+        context.fillText(printTXT + (currentNumber + 1), 30, this.canvas.height - 20);
+        if (resetGame) {
             this.resize(this.canvas.width, this.canvas.height);
         }
         this.connectParticles(context);
         this.connectSelected(context);
-         
+
         ctx.font = "bold 20px Courier";
         let countSelected = 0;
         this.particles.forEach(particle => {
+
+            context.strokeStyle = 'blue';
             particle.draw(context);
             particle.update();
-            if(particle.isSelected) countSelected++;
+            if (particle.isSelected) countSelected++;
         });
-        
-        if(countSelected == this.numberOfParticles){
+
+        if (countSelected == this.numberOfParticles) {
             this.level++; // NEXT LEVEL
             this.particles = [];
             this.numberOfParticles = this.level * 10;
@@ -148,34 +157,34 @@ class Player {
             this.resize(this.canvas.width, this.canvas.height);
         }
     }
-    connectSelected(context){
+    connectSelected(context) {
         context.save();
         context.strokeStyle = 'green';
-        
-        for (let a = 0; a < this.particles.length -1; a++){
-            if(this.particles[a].isSelected && this.particles[a + 1].isSelected){
-               context.strokeWidth = 15;
-                    context.beginPath();
-                    context.moveTo(this.particles[a].x, this.particles[a].y);
-                    context.lineTo(this.particles[a + 1].x, this.particles[a + 1].y);
-                    context.lineTo(this.particles[a].x + 2, this.particles[a].y + 2);
-                    context.stroke();
+
+        for (let a = 0; a < this.particles.length - 1; a++) {
+            if (this.particles[a].isSelected && this.particles[a + 1].isSelected) {
+                context.strokeWidth = 15;
+                context.beginPath();
+                context.moveTo(this.particles[a].x, this.particles[a].y);
+                context.lineTo(this.particles[a + 1].x, this.particles[a + 1].y);
+                context.lineTo(this.particles[a].x + 2, this.particles[a].y + 2);
+                context.stroke();
             }
         }
-        context.restore(); 
+        context.restore();
     }
-    connectParticles(context){
+    connectParticles(context) {
         const maxDistance = 300;
-        
+
         context.strokeStyle = 'white';
-        for (let a = 0; a < this.particles.length; a++){
-            for (let b = a; b < this.particles.length; b++){
+        for (let a = 0; a < this.particles.length; a++) {
+            for (let b = a; b < this.particles.length; b++) {
                 const dx = this.particles[a].x - this.particles[b].x;
                 const dy = this.particles[a].y - this.particles[b].y;
                 const distance = Math.hypot(dx, dy);
-                if (distance < maxDistance){
+                if (distance < maxDistance) {
                     context.save();
-                    const opacity = 1 - (distance/maxDistance);
+                    const opacity = 1 - (distance / maxDistance);
                     context.globalAlpha = opacity;
                     context.beginPath();
                     context.moveTo(this.particles[a].x, this.particles[a].y);
@@ -186,14 +195,14 @@ class Player {
             }
         }
     }
-    resize(width, height){
+    resize(width, height) {
         resetGame = false;
         currentNumber = 1;
         this.canvas.width = width;
         this.canvas.height = height;
         this.width = width;
         this.height = height;
-        const gradient = this.context.createLinearGradient(0,0, width, height);
+        const gradient = this.context.createLinearGradient(0, 0, width, height);
         gradient.addColorStop(0, 'white');
         gradient.addColorStop(0.5, 'gold');
         gradient.addColorStop(1, 'orangered');
@@ -206,7 +215,7 @@ class Player {
 }
 const player = new Player(canvas, ctx);
 
-function animate(){
+function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.updateParticles(ctx);
     requestAnimationFrame(animate);
